95f10df1cae97e33b74371dedcdc6b58e1888abd
Likio3000 <abethuneplou@gmail.com>
2023-11-04 21:53:23 +0100

    api connections

--- run_app.bat
+++ run_app.bat
@@ -1,4 +1,5 @@
 python main.py
 C:\Users\likio\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\streamlit run src/stream.py
+streamlit run C:/Users/jbethune/Desktop/ML_introbook/Portfolio_AlexBP/src/flutte.py
 
-#
+
